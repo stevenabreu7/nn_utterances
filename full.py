52aefbbc5a44cee9be7fd62f1f447c71b2d8a025
Steven Abreu <stevenabreu7@gmail.com>
2018-09-24 18:12:15 -0400

    + fix

--- full.py
+++ full.py
@@ -4,37 +4,6 @@ import tensorflow as tf
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 
-# general initialization
-padding, context = 10, 10
-batch_size = 5000
-trainer = Trainer('data/train.npy', 'data/train_labels.npy', 'data/dev.npy', 'data/dev_labels.npy', padding, context, batch_size)
-
-# model - 200x200x200
-name = 'test_model'
-net = torch.nn.Sequential(
-    torch.nn.Linear(trainer.input_dim, 200),
-    torch.nn.BatchNorm1d(200),
-    torch.nn.LeakyReLU(),
-    torch.nn.Linear(200, 200),
-    torch.nn.BatchNorm1d(200),
-    torch.nn.LeakyReLU(),
-    torch.nn.Linear(200, 200),
-    torch.nn.BatchNorm1d(200),
-    torch.nn.LeakyReLU(),
-    torch.nn.Linear(200, trainer.output_dim)
-)
-epochs = 10
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.3)
-scheduler = None
-logging = True
-# save it into a model
-model = Model(name, net, epochs, criterion, optimizer, scheduler, logging)
-# train the model
-trainer.training_routine(model)
-
 class Trainer():
     def __init__(self, trainx, trainy, valx, valy, padding, context, batch_size):
         self.input_dim = 40 * (2 * padding + 1)
@@ -257,3 +226,34 @@ class Model():
         self.optimizer = optimizer
         self.scheduler = scheduler 
         self.logging = logging
+
+# general initialization
+padding, context = 10, 10
+batch_size = 5000
+trainer = Trainer('data/train.npy', 'data/train_labels.npy', 'data/dev.npy', 'data/dev_labels.npy', padding, context, batch_size)
+
+# model - 200x200x200
+name = 'test_model'
+net = torch.nn.Sequential(
+    torch.nn.Linear(trainer.input_dim, 200),
+    torch.nn.BatchNorm1d(200),
+    torch.nn.LeakyReLU(),
+    torch.nn.Linear(200, 200),
+    torch.nn.BatchNorm1d(200),
+    torch.nn.LeakyReLU(),
+    torch.nn.Linear(200, 200),
+    torch.nn.BatchNorm1d(200),
+    torch.nn.LeakyReLU(),
+    torch.nn.Linear(200, trainer.output_dim)
+)
+epochs = 10
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.3)
+scheduler = None
+logging = True
+# save it into a model
+model = Model(name, net, epochs, criterion, optimizer, scheduler, logging)
+# train the model
+trainer.training_routine(model)
