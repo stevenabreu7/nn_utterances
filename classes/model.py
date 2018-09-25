793845885f1df1dacb95e95c5e56a1be8baa303c
Steven Abreu <stevenabreu7@gmail.com>
2018-09-24 23:25:59 -0400

    + removed class folder

--- classes/model.py
+++
@@ -1,9 +0,0 @@
-class Model():
-    def __init__(self, name, net, epochs, criterion, optimizer, scheduler, logging):
-        self.name = name 
-        self.net = net 
-        self.epochs = epochs 
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.scheduler = scheduler 
-        self.logging = logging
