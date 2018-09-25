d442f57cd8bde7bddd13f7a2b934b142454f0fc2
Steven Abreu <stevenabreu7@gmail.com>
2018-09-25 15:59:07 -0400

    + another type fix?

--- new.py
+++ new.py
@@ -202,10 +202,10 @@ class Trainer:
 
                 # print training progress
                 if batch_i % 10 == 0:
-                    print('\rEpoch {:3} Progress {:5.2%} Accuracy {:5.2%}'.format(
+                    print('\rEpoch {:3} Progress {:6.3%} Accuracy {:6.3%}'.format(
                         epoch + 1, 
                         batch_i * self.train_loader.batch_size / len(self.train_loader.dataset),
-                        train_correct / train_num
+                        train_correct.cpu().item() / train_num
                     ), end='')
                 
             # NOTE do we need this?
